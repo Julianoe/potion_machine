7fff30e6391b8eb901430176a22f965b17f946af
Julien <ju.gasba@gmail.com>
2016-03-23 18:50:31 +0100

    ajout des valeurs

--- frondlock_potion_machine.js
+++ frondlock_potion_machine.js
@@ -26,15 +26,31 @@ function getCouleur(){
   var couleur = rpg_data.couleurs[index];
   return couleur;
 }
+function getDuree(){
+  var index = Math.floor(Math.random()*rpg_data.durees.length)
+  var duree = rpg_data.durees[index];
+  return duree;
+}
+function getValeur(){
+  var valeur = Math.floor(Math.random() * (25 - 5 + 1)) + 5;
+  return valeur;
+}
+
 // mon générateur de potion
 function generateFrondlockPotion() {
     var couleur = getCouleur();
     var niveau = getNiveau();
     var carac = getCaracteristic();
-    var article = "A ";
+    var duree = getDuree();
+    var valeur = getValeur();
     var potion = "Une potion de " + carac + " " + niveau + " de couleur " + couleur + ". ";
-    return potion;
+    var effet = "<p><strong>Effet :</strong> Bonus de " + valeur + " de " + carac + ".</p>" ;
+    var resultat = potion + effet ;
+    return resultat;
 }
+
+
+
 // envoi de la potion Frondlock dans le fichier html
 function renderFrondlockPotion() {
     var potion = generateFrondlockPotion();
