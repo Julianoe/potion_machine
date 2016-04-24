c2ae013f748a581834e52a4b6b8e2ee6e67bd0b9
Julien <ju.gasba@gmail.com>
2016-04-24 21:12:05 +0200

    adding working checkboxes to choose the levels of potions

--- frondlock_potion_machine.js
+++ frondlock_potion_machine.js
@@ -18,8 +18,65 @@ function getCaracteristique() {
   return caracteristique;
 }
 function getPuissance(){
-  var niveau = rpg_data.niveaux
-  var raretee = rpg_data.niveauxPoids
+  var niveau = [];
+  var raretee = [];
+
+  // this functions searches the presence of the value in an array
+  function in_array(array, value) {
+    for(var i=0;i<array.length;i++) {
+        return (array[i][0].id === value)
+    }
+    return false;
+  }
+
+  // these functions create entries in an array according to the checked box
+  if ( document.getElementById("check_mythique").checked ){
+    if ( in_array(niveau, "mythique") == false ){
+      niveau.push('mythique');
+      raretee.push('5');
+    }
+  }
+  if ( document.getElementById("check_puissante").checked ){
+    if ( in_array(niveau, "puissante") == false){
+      niveau.push('puissante');
+      raretee.push('10');
+    }
+  }
+  if ( document.getElementById("check_majeure").checked ){
+    if ( in_array(niveau, "majeure") == false){
+      niveau.push('majeure');
+      raretee.push('10');
+    }
+  }
+  if ( document.getElementById("check_intermediaire").checked ){
+    if ( in_array(niveau, "intermediaire") == false){
+      niveau.push('intermediaire');
+      raretee.push('15');
+    }
+  }
+  if ( document.getElementById("check_standard").checked ){
+    if ( in_array(niveau, "standard") == false){
+      niveau.push('standard');
+      raretee.push('150');
+    }
+  }
+  if ( document.getElementById("check_faible").checked ){
+    if ( in_array(niveau, "faible") == false ){
+      niveau.push('faible');
+      raretee.push('15');
+    }
+  }
+  if ( document.getElementById("check_minime").checked ){
+    if ( in_array(niveau, "minime") == false ){
+      niveau.push('minime');
+      raretee.push('5');
+
+    }
+  }
+
+  // Ces deux entrees dans le .json ne sont pas utilisées
+  // var niveau = rpg_data.niveaux
+  // var raretee = rpg_data.niveauxPoids
 
   var totalraretee=eval(raretee.join("+")) //get total weight
   var puissance=new Array() //new array to hold "weighted" niveaux
@@ -34,11 +91,15 @@ function getPuissance(){
 
   return puissance[randomnumber];
 }
+
+// randomly picks a color
 function getCouleur(){
   var index = Math.floor(Math.random()*rpg_data.couleurs.length)
   var couleur = rpg_data.couleurs[index];
   return couleur;
 }
+// randomly picks an effect
+// this not currently used
 function getEffets(){
   var index = Math.floor(Math.random()*rpg_data.effets.length)
   var effets = rpg_data.effets[index];
@@ -51,7 +112,7 @@ function getValeurSimple(){
   return valeursimple;
 }
 
-// génère un valeur aléatoire comprise entre le maximum et le minimum entrés en paramètres
+// génère une valeure aléatoire comprise entre le maximum et le minimum entrés en paramètres
 function getValeur(max, min){
   var valeur = Math.floor(Math.random() * (max - min + 1)) + min;
   return valeur;
@@ -72,13 +133,13 @@ function getTemps(){
 /////////////////////////////////////////////////////////////
 
 
-function generateName(){
-  var puissance = getPuissance;
-  var carac = getCaracteristique();
-  var effet = getEffets();
-  return puissance;
-  // echo(puissance);
-}
+// function generateName(){
+//   var puissance = getPuissance;
+//   var carac = getCaracteristique();
+//   var effet = getEffets();
+//   return puissance;
+//   // echo(puissance);
+// }
 //envoi du nom de la potion dans le fichier html
 // function renderTitlePotion() {
 //   var titre = generateName() ;
@@ -96,65 +157,62 @@ function generateFrondlockPotion() {
     // var effets = getEffets();
     var puissance = getPuissance();
 
-    // if (puissance == 'mythique' & carac == 'force'){
-    //   var name = "Une Potion de " + carac + " de géant<br><br>";
-    // }
-    // else{
-    //   var name = "Potion classique<br><br>";
-    // }
+
+    // conditionnaly verify if it is a special potion
+    if (puissance == 'mythique' & carac == 'force'){
+      var name = "Une Potion de " + carac + " de géant<br><br>";
+    }
+    else{
+      var name = "Potion classique<br><br>";
+    }
 
 
-    // tester les checkbox
-      // quel est le
-      var bonuschecked = document.getElementById("check_bonus").checked;
-      var maluschecked = document.getElementById("check_malus").checked;
-      if (maluschecked && bonuschecked){
-        var effets = getEffets();
-      }
-      else if ( maluschecked && bonuschecked==false ){
-        effets = "malus";
-      }
-      else if ( bonuschecked && maluschecked==false ){
-        effets = "bonus";
-      }
-      else if ( bonuschecked == false && maluschecked == false ) {
-        var effets = getEffets();
-      }
+    // Check the checked
+    var allchecked = document.getElementById("check_all").checked;
+    var bonuschecked = document.getElementById("check_bonus").checked;
+    var maluschecked = document.getElementById("check_malus").checked;
+
+    if ( bonuschecked ){
+      effets = "bonus" ;
+    }
+    else if ( maluschecked ){
+      effets = "malus" ;
+    }
+    else {
+      var effets = getEffets();
+    }
+
+    if(effets == "bonus" ){
+      var operande = "+";
+    }
+    else if (effets == "malus"){
+      var operande = "-";
+    }
+
+    if(puissance=="mythique"){
+        var valeur = getValeur(25, 18);
+    }
+    else if (puissance=="puissante") {
+      var valeur = getValeur(17, 14);
+    }
+    else if (puissance=="majeure") {
+      var valeur = getValeur(13, 9);
+    }
+    else if (puissance=="intermediaire") {
+      var valeur = getValeur(8, 6);
+    }
+    else if (puissance=="standard") {
+      var valeur = getValeur(6, 4);
+    }
+    else if (puissance=="faible") {
+      var valeur = getValeur(4, 2);
+    }
+    else if (puissance=="minime") {
+      var valeur = getValeur(2, 1);
+    }
 
 
-
-
-      if(effets == "bonus" ){
-        var operande = "+";
-      }
-      else if (effets == "malus"){
-        var operande = "-";
-      }
-
-      if(puissance=="mythique"){
-          var valeur = getValeur(25, 18);
-      }
-      else if (puissance=="puissante") {
-        var valeur = getValeur(17, 14);
-      }
-      else if (puissance=="majeure") {
-        var valeur = getValeur(13, 9);
-      }
-      else if (puissance=="intermediaire") {
-        var valeur = getValeur(8, 6);
-      }
-      else if (puissance=="standard") {
-        var valeur = getValeur(6, 4);
-      }
-      else if (puissance=="faible") {
-        var valeur = getValeur(4, 2);
-      }
-      else if (puissance=="minime") {
-        var valeur = getValeur(2, 1);
-      }
-
-
-    var potion = "Une potion de <strong>" + effets + "</strong> de <br><strong>" + carac + "</strong><br/> <strong>" + puissance + "</strong><br> de couleur <strong>" + couleur + "</strong>. ";
+    var potion = "Une potion de <strong>" + effets + "</strong> de <strong>" + carac + "</strong> <strong>" + puissance + "</strong> de couleur <strong>" + couleur + "</strong>. ";
     var effet = "<br><br><strong>Effet :</strong> " + operande   + valeur + " de " + carac ;
     var resultat = name + potion + effet + " pendant " + temps + ".";
 
